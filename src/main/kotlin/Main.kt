4f875ffe80f8a2772e3c02ba5e76932af3596776
송효범 <hyobum.song@spiceware.io>
2023-04-01 16:26:28 +0900

    Main 수정
    불필요 코드 삭제

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,3 @@
 fun main(args: Array<String>) {
     solution1000()
-//    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-//    println("Program arguments: ${args.joinToString()}")
 }
